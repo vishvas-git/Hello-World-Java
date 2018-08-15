17edb0ce3c75a1fc207f8c868f8dd869538cd478
vish <vishvas.srivastava@hotmail.co.uk>
2018-08-16 00:33:40 +0530

    Print msg updated

--- docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
+++ docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -7,7 +7,7 @@ package org.pictolearn.docker;
 public class HelloWorldPing {
 	public static void main(String[] args) throws InterruptedException {
 		for (int i = 0; i < 100; i++) {
-			System.out.println("Hello World! " + i);
+			System.out.println("Java->GitHub->DockerHub(CI)|Hello World! " + i);
 			Thread.sleep(1000);
 		}
 	}
